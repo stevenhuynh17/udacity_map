69a93e2f5874a606606b9ad2381ad175d3ace3bd
Steven <stevenhuynh17@gmail.com>
2018-02-23 10:14:17 -0800

    got the list working

--- js/app.js
+++ js/app.js
@@ -3,72 +3,116 @@ function initMap() {
     center: {lat: 37.7215546, lng: -122.3028448},
     zoom: 11
   });
+  bartAPI();
+}
 
-  ko.applyBindings(new AppViewModel());
+ko.bindingHandlers.autoSearch = {
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = valueAccessor();
+        var query = ko.unwrap(value).toLowerCase();
+        var stations = viewModel.stations();
+
+
+        if(query) {
+          for(var i = 0; i < stations.length; i++) {
+            var station = viewModel.stations()[i].name().name.toLowerCase();
+            var match = station.substr(0, query.length);
+            if(query === match) {
+              viewModel.stations()[i].shouldShow(true);
+            } else {
+              viewModel.stations()[i].shouldShow(false);
+            }
+          }
+        } else {
+          for(var i = 0; i < stations.length; i++) {
+            viewModel.stations()[i].shouldShow(true);
+          }
+        }
+    }
+};
+
+function apply(data) {
+  ko.applyBindings(new StationsViewModel(data));
+  var test = populateMap(data);
 }
 
 function bartAPI() {
-  // Array of markers for them to be placed on the map
-  var markers = [];
-  return $.ajax({
+  var info = $.ajax({
     url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y",
     dataType: "json",
-    success: function(data) {
-      console.log(data);
+  }).done(apply);
+}
 
-      // Extracting information for each station
-      var stations = data.root.stations.station;
-      var infowindow = new google.maps.InfoWindow();
+function StationsViewModel(data) {
+  var stations = data.root.stations.station;
+  this.query = ko.observable("");
+  this.stations = ko.observableArray(createList(stations));
+}
 
-      // For each station, extract the coordinates to create a new instance
-      // of a google map marker
-      for(i = 0; i < stations.length; i++) {
-        var lat = Number(stations[i].gtfs_latitude);
-        var lng = Number(stations[i].gtfs_longitude);
-        var title = stations[i].name;
+//Functions below are good to go
 
-        var marker = new google.maps.Marker({
-            position: {lat: lat, lng: lng},
-            map: map,
-            title: title
-        })
+function createList(data) {
+  var list =[];
+  for(var i = 0; i < data.length; i++) {
+    list.push(new StationInformation(data[i]));
+  }
+  return list;
+}
 
-        markers.push(marker);
-        marker.addListener('click', function() {
-          populateInfoWindow(this, infowindow);
-        });
-      }
+function StationInformation(name) {
+  this.shouldShow = ko.observable(true);
+  this.name = ko.observable(name);
+}
 
-      function populateInfoWindow(marker, infowindow) {
-        infowindow.setContent(marker.title)
-        infowindow.open(map, marker)
-      }
-      return data;
+function populateMap(data) {
+  // Extracting information for each station
+  var markers = [];
+  var stations = data.root.stations.station;
+  var infowindow = new google.maps.InfoWindow();
+
+  // For each station, extract the coordinates to create a new instance
+  // of a google map marker
+  for(i = 0; i < stations.length; i++) {
+    var lat = Number(stations[i].gtfs_latitude);
+    var lng = Number(stations[i].gtfs_longitude );
+    var title = stations[i].name;
+
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: title
+    })
+
+    markers.push(marker);
+    marker.addListener('click', function() {
+      populateInfoWindow(this, infowindow);
+      toggleBounce(this);
+    });
+  }
+
+  function toggleBounce(marker) {
+    for(var i = 0; i < markers.length; i++) {
+      markers[i].setAnimation(null);
     }
-  });
+
+    if(marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+
+  function populateInfoWindow(marker, infowindow) {
+    infowindow.close(map, marker);
+    infowindow.setContent(marker.title);
+    infowindow.open(map, marker);
+  }
+  return markers;
 }
 
-function SeatReservation(name, initialMeal) {
-    var self = this;
-    self.name = name;
-    self.meal = ko.observable(initialMeal);
-}
-
-function AppViewModel() {
-  var self = this;
-
-    // // Non-editable catalog data - would come from the server
-    // self.availableMeals = [
-    //     { mealName: "Standard (sandwich)", price: 0 },
-    //     { mealName: "Premium (lobster)", price: 34.95 },
-    //     { mealName: "Ultimate (whole zebra)", price: 290 }
-    // ];
-    //
-    // // Editable data
-    // self.seats = ko.observableArray([
-    //     new SeatReservation("Steve", self.availableMeals[0]),
-    //     new SeatReservation("Bert", self.availableMeals[0])
-    // ]);
-  console.log("Working");
-  console.log(bartAPI());
+function showInfo(test) {
+  var content = 1;
+  console.log("TESTING");
+  console.log(google.maps.event.trigger(this));
 }
