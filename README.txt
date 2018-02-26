dfab5125a0a22a6b877c673c0793e7d0adddb649
Steven <stevenhuynh17@gmail.com>
2018-02-26 13:32:53 -0800

    Everything is functioning

---
+++ README.txt
@@ -0,0 +1,24 @@
+—————————————
+INTRODUCTION:
+—————————————
+PROJECT: Neighborhood map
+
+DESCRIPTION: A map utilizing the Google Maps API that is populated with markers of BART stations
+
+AUTHORS: Steven Huynh
+
+
+———————————————————————
+OPERATING INSTRUCTIONS:
+———————————————————————
+Double click on the app.html file. If that doesn’t work for some reason, try right clicking on the file and select the browser of your choice to open.
+
+
+—————————
+CONTENTS:
+—————————
+- app.html
+- style.css
+- app.js
+- knockout-3.4.2.js
+- README.txt
--- js/app.js
+++ js/app.js
@@ -1,26 +1,32 @@
+//Loads up the map with the intial location of the region
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7215546, lng: -122.3028448},
     zoom: 11
   });
+  //Ajax call to populate the map with markers of the respective BART stations
   bartAPI();
 }
 
+// Custom handlers section where autoSearch does an autocomplete when searching
+// for stations
 ko.bindingHandlers.autoSearch = {
     update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
         var value = valueAccessor();
         var query = ko.unwrap(value).toLowerCase();
         var stations = viewModel.stations();
 
-
         if(query) {
           for(var i = 0; i < stations.length; i++) {
-            var station = viewModel.stations()[i].name().name.toLowerCase();
+            var station = stations[i].name().toLowerCase();
             var match = station.substr(0, query.length);
+
             if(query === match) {
-              viewModel.stations()[i].shouldShow(true);
+              stations[i].shouldShow(true);
+
             } else {
-              viewModel.stations()[i].shouldShow(false);
+              stations[i].shouldShow(false);
             }
           }
         } else {
@@ -31,11 +37,7 @@ ko.bindingHandlers.autoSearch = {
     }
 };
 
-function apply(data) {
-  ko.applyBindings(new StationsViewModel(data));
-  var test = populateMap(data);
-}
-
+// BART AJAX API call
 function bartAPI() {
   var info = $.ajax({
     url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y",
@@ -43,27 +45,28 @@ function bartAPI() {
   }).done(apply);
 }
 
+// Applies bindings to utilize the Knockout Framework with StationsViewModel by
+// passing down data retreived from the API call
+function apply(data) {
+  ko.applyBindings(new StationsViewModel(data));
+}
+
+// View Model for the list of stations to be displayed
 function StationsViewModel(data) {
   var stations = data.root.stations.station;
   this.query = ko.observable("");
-  this.stations = ko.observableArray(createList(stations));
+  this.stations = ko.observableArray(populateMap(data));
 }
 
-//Functions below are good to go
-
-function createList(data) {
-  var list =[];
-  for(var i = 0; i < data.length; i++) {
-    list.push(new StationInformation(data[i]));
-  }
-  return list;
-}
-
-function StationInformation(name) {
+// View Model for the individual station
+function StationInformation(name, marker, info) {
+  this.marker = ko.observable(marker);
   this.shouldShow = ko.observable(true);
   this.name = ko.observable(name);
+  this.showInfo = info;
 }
 
+// Adds the markers to the Google Maps
 function populateMap(data) {
   // Extracting information for each station
   var markers = [];
@@ -84,18 +87,31 @@ function populateMap(data) {
         title: title
     })
 
-    markers.push(marker);
+    // Allows the marker to be clicked where it'll bounce and display info
     marker.addListener('click', function() {
       populateInfoWindow(this, infowindow);
       toggleBounce(this);
     });
+    // Create a new instance of each station that will be stored in an array
+    markers.push(new StationInformation(title, marker, connectInfo));
   }
 
-  function toggleBounce(marker) {
-    for(var i = 0; i < markers.length; i++) {
-      markers[i].setAnimation(null);
-    }
+  // Enables the list of stations to be clicked and interact with the map
+  function connectInfo(info) {
+    toggleBounce(info.marker());
+    populateInfoWindow(info.marker(), infowindow)
+  }
 
+  // Makes the marker bounce when clicked
+  function toggleBounce(marker) {
+    // Stops all markers from bouncing
+    for(var i = 0; i < markers.length; i++) {
+      var obj = markers[i].marker();
+      if(obj.getAnimation() != null) {
+        obj.setAnimation(null);
+      }
+    }
+    // Only allow one marker to bounce at a time
     if(marker.getAnimation() !== null) {
       marker.setAnimation(null);
     } else {
@@ -103,16 +119,12 @@ function populateMap(data) {
     }
   }
 
+  // Displays information of the marker when clicked
   function populateInfoWindow(marker, infowindow) {
     infowindow.close(map, marker);
     infowindow.setContent(marker.title);
     infowindow.open(map, marker);
   }
-  return markers;
-}
 
-function showInfo(test) {
-  var content = 1;
-  console.log("TESTING");
-  console.log(google.maps.event.trigger(this));
+  return markers;
 }
